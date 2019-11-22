48c8f4a3484ac0a9a9be271c4b12b05207e3262a
--add <--add>
2019-11-22 17:43:27 -0500

    random winner game

--- randomWinner.js
+++ randomWinner.js
@@ -1,3 +1,12 @@
+/*Suppose 10 persons are sitting in a circle starting from position 1 to position
+10. A pillow with some random number written on it is given to the person at position 1.
+Now Person at position 1 has to add that particular number written on pillow with his 
+position number i.e 1. And pass that pillow to the person at position 2 and he also has to add
+its position  number with number written on pillow and pass the pillow to the person at position 
+3 and so on to the person 10. The Winner of that round are those people who are having a Prime
+number (the number after addition). And now you have to calculate the amount distributed as Prize.
+So the amount will be the total of all the winners Prime number which we just calculated.
+*/
 function randomWinner(){
     let randomNumber=Math.ceil(Math.random()*10);
 let arr=[1,2,3,4,5,6,7,8,9,10];
